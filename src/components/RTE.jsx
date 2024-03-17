39170dc5d5057ad0eebda3e59ea030de68148851
Rimon Hossain <rimonhossen333@gmail.com>
2024-03-17 15:27:46 +0600

    second commit

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,7 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey='jjpp8heljhlg5aca9pyxtrb4d5o66e6maf37rr57qn3kq4rf'
+        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
